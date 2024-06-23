d87af5e5e14ea682d29080905d5cb4a772cc6a2c
martinasladek <m.sladekova@sussex.ac.uk>
2024-06-23 15:19:30 +0100

    add export location

--- git_upload_prep.R
+++ git_upload_prep.R
@@ -1,5 +1,9 @@
 for(i in list.dirs()[stringr::str_detect(list.dirs(), "simulation_exports")]){
   
- writeLines("", con = paste0(i, "/export_location.txt"))
+ writeLines(
+   paste0(
+          "All files can be accessed at: https://sussex.box.com/s/wgsemizl7ulv4zt53zxi0tyr38n6myeb \n\nContact Martina at m.sladekova@sussex.ac.uk if you encounter problems accessing the files"
+          ), 
+   con = paste0(i, "/export_location.txt"))
   
 }
